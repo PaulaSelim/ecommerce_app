556204e18c93c1a327d27fd2f4e521d4baf7dfcc
PaulaSelim <s.paulakhalil@icloud.com>
2024-09-29 22:41:37 +0300

    added search bar, message, hot picks section, and see all

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,10 +10,68 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Center(
-        child: Text('Shop Page'),
-      ),
+    return Column(
+      children: [
+        //search bar
+        Container(
+          margin: const EdgeInsets.all(13),
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Search',
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              Icon(
+                Icons.search,
+              ),
+            ],
+          ),
+        ),
+
+        //message
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+            'every one flies.. some fly longer than others',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ),
+
+        //hot picks
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'See All',
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
