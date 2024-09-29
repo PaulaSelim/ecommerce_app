e80a9773f3b691b0c4a62a9e91412797d288522e
PaulaSelim <s.paulakhalil@icloud.com>
2024-09-30 00:19:12 +0300

    added ShoeTile component to display a shoe item with image, description, price, and add to cart button

---
+++ lib/components/shoe_tile.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import '../modules/shoe.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 25.0),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // shoe image
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(shoe.imagePath),
+          ),
+          // description
+          Text(
+            shoe.description,
+            style: TextStyle(
+              color: Colors.grey[600],
+            ),
+          ),
+          // price + details
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 13.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // shoe name
+                    Text(
+                      shoe.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+
+                    const SizedBox(height: 5),
+
+                    //price
+                    Text(
+                      '\$' + shoe.price,
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              // plus button
+              Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.add,
+                ),
+              ),
+            ],
+          ),
+          // button to add to cart
+        ],
+      ),
+    );
+  }
+}
