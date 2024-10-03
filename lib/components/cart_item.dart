f991025ba682bd6eaa9ec67a8faff953fd68bb71
PaulaSelim <s.paulakhalil@icloud.com>
2024-10-03 19:18:01 +0300

    implemented cart item logic and UI

---
+++ lib/components/cart_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../modules/cart.dart';
+import '../modules/shoe.dart';
+
+class CartItem extends StatefulWidget {
+  Shoe shoe;
+  CartItem({
+    super.key,
+    required this.shoe,
+  });
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  // remove item from cart
+  void removeItemFromCart() {
+    Provider.of<Cart>(context, listen: false).removeItem(widget.shoe);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(widget.shoe.imagePath),
+      title: Text(widget.shoe.name),
+      subtitle: Text(widget.shoe.price),
+      trailing: IconButton(
+        icon: Icon(Icons.delete),
+        onPressed: removeItemFromCart,
+      ),
+    );
+  }
+}
