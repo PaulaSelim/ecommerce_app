d9f109f99e9eec7e13406fd64eba8cda9f92d9e3
PaulaSelim <s.paulakhalil@icloud.com>
2024-10-03 19:29:56 +0300

    removed unnecessary code

--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,29 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
+// ignore: must_be_immutable
 class MyBottomNavBar extends StatelessWidget {
   void Function(int)? onTabChange;
   MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: const EdgeInsets.only(bottom: 18),
-        child: GNav(
-          mainAxisAlignment: MainAxisAlignment.center,
-          onTabChange: (value) => onTabChange!(value),
-          tabs: const [
-            GButton(
-              icon: Icons.home,
-              // text: 'Home',
-            ),
-            GButton(
-              icon: Icons.shopping_cart,
-              // text: 'Cart',
-            ),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 18),
+      child: GNav(
+        mainAxisAlignment: MainAxisAlignment.center,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: const [
+          GButton(
+            icon: Icons.home,
+            // text: 'Home',
+          ),
+          GButton(
+            icon: Icons.shopping_cart,
+            // text: 'Cart',
+          ),
+        ],
       ),
     );
   }
