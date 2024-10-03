d615d983df1e67a745055120eb55b493b55e34dd
PaulaSelim <s.paulakhalil@icloud.com>
2024-10-03 19:18:38 +0300

    finished cart page UI and logic

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../components/cart_item.dart';
+import '../modules/cart.dart';
+import '../modules/shoe.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -10,9 +15,36 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Center(
-        child: Text('Cart Page'),
+    return Consumer<Cart>(
+      builder: (context, value, child) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              "My Cart",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: value.getCart().length,
+              itemBuilder: (context, index) {
+                //get individual shoe
+                Shoe individualShow = value.getCart()[index];
+
+                //return the cart item
+                return CartItem(
+                  shoe: individualShow,
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
