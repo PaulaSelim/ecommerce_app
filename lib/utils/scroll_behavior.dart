e738b152de80c46dc6da18dbced9bf62073dd6e6
PaulaSelim <s.paulakhalil@icloud.com>
2024-10-05 00:12:37 +0300

    Add MyCustomScrollBehavior class for customizing scroll behavior throught the application

---
+++ lib/utils/scroll_behavior.dart
@@ -0,0 +1,14 @@
+// scroll_behavior.dart
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class MyCustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.unknown,
+      };
+}
